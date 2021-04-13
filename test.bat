1c6587b808c37de248dbf303fbff5bb1beddf2ec
Hamed <62185214+hamedlav@users.noreply.github.com>
2021-04-13 12:47:13 +0430

    Add files via upload

---
+++ test.bat
@@ -0,0 +1,3 @@
+C:\Users\hamed.l\AppData\Local\Programs\Python\Python38\python.exe "D:/Local Disk/Python/Tamrin/Advanced/6th Chapter/homefinal.py"
+ECHO Congratulations! Your first batch file executed successfully.
+PAUSE
